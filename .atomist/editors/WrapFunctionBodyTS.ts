b6236a4879ac1c26117e3cbc81e653eb41eb046e
Jessica Kerr <jessitron@gmail.com>
2017-02-17 16:52:52 +0000

    works better

--- .atomist/editors/WrapFunctionBodyTS.ts
+++ .atomist/editors/WrapFunctionBodyTS.ts
@@ -5,27 +5,38 @@ import { PathExpressionEngine, Microgrammar } from '@atomist/rug/tree/PathExpres
 import { Editor, Parameter, Tags } from '@atomist/rug/operations/Decorators'
 
 @Editor("WrapFunctionBodyTS", "change the type, and wrap the body, of certain functinos")
-@Tags("documentation")
+@Tags("scala")
 class WrapFunctionBodyTS {
 
     edit(project: Project) {
 
       let mg = new Microgrammar('syntaxNodeParser',
-       `: ⟦Parser[SyntaxNode]⟧ = $body`,
-       {  body: `$endOfBody`,
-          endOfBody: `¡⟦
+       `: $declaredType = $body`,
+       { declaredType: "Parser[SyntaxNode]",
+         body: everythingUpTo(literalWithWhitespace(`
 
-⟧¡`})
+`))})
       let eng: PathExpressionEngine = project.context().pathExpressionEngine().addType(mg)
-
- console.log("letsa go");
 
        eng.with<any>(project, `//File()[@name="ElmParser.scala"]/syntaxNodeParser()`, n => {
         console.log("Matched node:" + n.value())
-        console.log("with body: " + n.body().value())
+        let currentBody = n.body().value().trim();
+        n.declaredType().update(`Parser[PositionedSyntaxNode]`)
+        n.body().update(`positionedNode(${currentBody})
 
+`)
+        console.log("After:" + n.value())
       })
+
+      function everythingUpTo(matcher: String) {
+        return `¡${matcher}¡`;
+      }
+
+      function literalWithWhitespace(matcher: String) {
+        return `⟦${matcher}⟧`;
+      }
     }
+
 }
 
 export let wrapFunctionBodyTS = new WrapFunctionBodyTS()
